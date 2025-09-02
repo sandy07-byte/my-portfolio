ed0c913542f7bc0fcdbc1840ba5933b8cddb43ef
Sandeep Damera <sandeep.damera05@gmail.com>
2025-09-02 19:14:47 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './Components/navbar/Navbar'
 import Home from './Components/navbar/home/Home'
 import About from './Components/About/About'
-import Contact from './Components/contact/contact'
+import Contact from './Components/contact/Contact'
 import Footer from './Components/Footer/Footer'
 import Certification from './Components/Certifiactions/Certification'
 const App = () => {
