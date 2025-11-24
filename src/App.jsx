1bbaf244c2f81561399ac92596ca57df57d03f8f
Sandeep Damera <sandeep.damera05@gmail.com>
2025-11-24 10:07:16 +0530

    Fix Contact component import case sensitivity issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './Components/navbar/Navbar'
 import Home from './Components/navbar/home/Home'
 import About from './Components/About/About'
 import Projects from './Components/Projects/Projects'
-import Contact from './Components/contact/contact'
+import Contact from './Components/contact'
 import Footer from './Components/Footer/Footer'
 import Certification from './Components/Certifiactions/Certification'
 const App = () => {
